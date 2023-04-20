fe1d11872450e74ec21889357807e08e2f9220fb
Frank Garcia <garcia1901l@gmail.com>
2023-04-19 19:03:07 -0500

    test auth

---
+++ Modules/Auth/Tests/Feature/AuthTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Modules\Auth\Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * Test a successful user login.
+     *
+     * @return void
+     */
+    public function testUserLogin()
+    {
+        // Create a test user
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+
+        // Send a login request
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        // Check response status
+        $response->assertStatus(200);
+
+        // Check response data
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in',
+        ]);
+    }
+
+    /**
+     * Test a successful user registration.
+     *
+     * @return void
+     */
+    public function testUserRegistration()
+    {
+        // Generate random user data
+        $userData = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        // Send a registration request
+        $response = $this->postJson('/api/auth/register', $userData);
+
+        // Check response status
+        $response->assertStatus(201);
+
+        // Check response data
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in',
+        ]);
+    }
+
+    /**
+     * Test a successful user retrieval.
+     *
+     * @return void
+     */
+    public function testUserRetrieval()
+    {
+        // Create a test user
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+
+        // Generate a token for the user
+        $token = auth('api')->login($user);
+
+        // Send a user retrieval request
+        $response = $this->get('/api/auth/user', [
+            'Authorization' => 'Bearer ' . $token,
+        ]);
+
+        // Check response status
+        $response->assertStatus(200);
+
+        // Check response data
+        $response->assertJson([
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+    }
+}
---
+++ Modules/Auth/Tests/Unit/AuthTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Auth\Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
